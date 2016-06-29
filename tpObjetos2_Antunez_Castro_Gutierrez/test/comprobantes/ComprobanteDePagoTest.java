7286801b4aea60e8212eacefbc944ea9a257b48d
Fer <fer-xxx@hotmail.com>
2016-06-29 18:02:09 -0300

    compila

--- tpObjetos2_Antunez_Castro_Gutierrez/test/comprobantes/ComprobanteDePagoTest.java
+++ tpObjetos2_Antunez_Castro_Gutierrez/test/comprobantes/ComprobanteDePagoTest.java
@@ -23,24 +23,19 @@ public class ComprobanteDePagoTest {
 	
 	@Before
 	public void setUp() {
-		nroCuota = 5;
 		participanteMock = mock(Participante.class);
 		planMock = mock(PlanDeAhorro.class);
 		concesionariaMock = mock(Concesionaria.class);
-		modeloMock = mock(Modelo.class);
 		
 		when(planMock.getConcesionaria()).thenReturn(concesionariaMock);
-		when(planMock.getModelo()).thenReturn(modeloMock);
 		when(concesionariaMock.gastoAdministrativos()).thenReturn(150f);
 		when(concesionariaMock.montoDelSeguro(participanteMock,planMock.getModelo())).thenReturn(100f);
 		when(planMock.calcularAlicuota()).thenReturn(900f);
-		
-		comprobanteTest = new ComprobanteDePago(nroCuota, planMock, participanteMock);
 	}
 
 	
 	@Test
 	public void testComprobanteDePago() {
+		new ComprobanteDePago(5, planMock, participanteMock);
 	}
-
 }
